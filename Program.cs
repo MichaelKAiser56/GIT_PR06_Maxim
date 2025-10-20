3b1e8b6acf3ecc21e5828ad7e32642e561b1e48a
Karpov Maksim Vladimirovich <mkarpov648@gmail.com>
2025-10-21 00:32:36 +0300

    Добвавлен файл Program.cs с информацией о группе и ФИО

---
+++ Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Karpov_PR06_GIT
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Группа: ИС-23А");
+            Console.WriteLine("ФИО: Карпов Максим Владимирович");
+        }
+    }
+}
